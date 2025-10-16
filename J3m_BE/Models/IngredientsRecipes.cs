567d664771c3023eb02da9a7c3cd33e0847d7a39
mao <mowitz.almstedt@chasacademy.se>
2025-10-16 12:49:50 +0200

    Implemented base models

---
+++ J3m_BE/Models/IngredientsRecipes.cs
@@ -0,0 +1,8 @@
+namespace J3m_BE.Models;
+
+// Joined table for Ingredients and Recipes
+
+public class IngredientsRecipes
+{
+    
+}
---
+++ J3m_BE/Models/Recipes.cs
@@ -0,0 +1,6 @@
+namespace J3m_BE.Models;
+
+public class Recipes
+{
+    
+}
