76ac761e92fcced513a49c744a585ef351af87ab
mao <mowitz.almstedt@chasacademy.se>
2025-10-23 11:56:05 +0200

    Generic repository and interface implemented.

---
+++ J3m_BE/Repositories/Interfaces/IGenericRepository.cs
@@ -0,0 +1,15 @@
+using System.Linq.Expressions;
+
+namespace J3m_BE.Repositories.Interfaces;
+
+public interface IGenericRepository<T> where T : class
+{
+    Task<IEnumerable<T>> GetAllAsync();
+    Task<T?> GetByIdAsync(int id);
+    Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate);
+    Task AddAsync(T entity);
+    void Update(T entity);
+    void Delete(T entity);
+    Task<bool> ExistsAsync(int id);
+    Task SaveChangesAsync();
+}
