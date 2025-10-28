d5c2b010a8e3e97cac0447ed10d2e76393fa754b
mao <mowitz.almstedt@chasacademy.se>
2025-10-28 14:47:21 +0100

    Fixed program.cs syntax

--- J3m_BE/Program.cs
+++ J3m_BE/Program.cs
@@ -19,13 +19,13 @@ namespace J3m_BE
             // Add services to the container.
             builder.Services.AddJ3MCore();
 
-      builder.Services.AddControllers();
+            builder.Services.AddControllers();
 
-      // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-      builder.Services.AddEndpointsApiExplorer();
-      builder.Services.AddSwaggerGen();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
 
-      var app = builder.Build();
+            var app = builder.Build();
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
@@ -45,5 +45,4 @@ namespace J3m_BE
             app.Run();
         }
     }
-  }
 }
