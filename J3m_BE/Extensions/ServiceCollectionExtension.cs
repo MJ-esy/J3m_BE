185c38c76a9400120eecaa12db68173829965b9d
Camilla <Camilla.e.soderman@gmail.com>
2025-11-12 20:17:01 +0100

    AlleryServiceTests are almost done, still issue to get the test to run but need to work in other branch.

--- J3m_BE/Extensions/ServiceCollectionExtension.cs
+++ J3m_BE/Extensions/ServiceCollectionExtension.cs
@@ -4,6 +4,7 @@ using J3m_BE.Services;
 using J3m_BE.Services.Implementations;
 using J3m_BE.Services.Interfaces;
 
+
 namespace J3m_BE.Extensions;
 
 // -- ----------------------------------------------------------------------------
@@ -21,6 +22,7 @@ public static class ServiceCollectionExtension
         services.AddScoped<INutrientGroupRepository, NutrientGroupRepository>();
         services.AddScoped<IAllergyRepository, AllergyRepository>();
         services.AddScoped<IDietRepository, DietRepository>();
+
 
         // Add Services here
         services.AddScoped<IFoodGroupService, FoodGroupService>();
