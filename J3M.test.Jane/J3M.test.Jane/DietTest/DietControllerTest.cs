d9aa12322610b494ae412097396a8b01e222dade
jyneto <j-pm@live.se>
2025-11-13 14:59:15 +0100

    Unit test for DietService kontrollerar att DietService.GetByIdAsync fungerar som förväntat. När en diet finns i repository → tjänsten ska returnera en korrekt DietDto med rätt värden. När en diet inte finns → tjänsten ska kasta ett NotFoundDomainException så att felet kan

---
+++ J3M.test.Jane/J3M.test.Jane/DietTest/DietControllerTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Moq;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace J3M.test.Jane.DietTest
+{
+    public class DietControllerTest
+    {
+
+    }
+}
