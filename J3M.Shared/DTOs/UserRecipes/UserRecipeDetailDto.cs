3ad8ecee2be5fb26c337147f910b9916d27204db
Camilla <Camilla.e.soderman@gmail.com>
2025-11-20 12:51:44 +0100

    Added `UserRecipeDetailDto` and `UserRecipeDto` classes to represent detailed and simplified user recipe data, respectively.

---
+++ J3M.Shared/DTOs/UserRecipes/UserRecipeDetailDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace J3M.Shared.DTOs.UserRecipes
+{
+    public class UserRecipeDetailDto
+    {
+        public int RecipeId { get; set; }
+        public string RecipeName { get; set; } = string.Empty;
+        public string? Description { get; set; }
+        public bool IsFavorite { get; set; }
+        public int PrepTimeMinutes { get; set; }
+        public string? ImageUrl { get; set; }
+    }
+}
---
+++ J3M.Shared/DTOs/UserRecipes/UserRecipeDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace J3M.Shared.DTOs.UserRecipes
+{
+    public class UserRecipeDto
+    {
+        public int RecipeId { get; set; }
+        public string RecipeName { get; set; } = string.Empty;
+        public bool IsFavorite { get; set; }
+    }
+}
