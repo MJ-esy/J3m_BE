8642bf6d1ad39a2d153bbe322d77b96d3a8a9f88
Camilla <Camilla.e.soderman@gmail.com>
2025-11-20 11:30:06 +0100

    IUserRecipeRepository

---
+++ J3m_BE/Repositories/Interfaces/IUserRecipeRepositorycs.cs
@@ -0,0 +1,13 @@
+﻿using J3m_BE.Models.Links;
+
+namespace J3m_BE.Repositories.Interfaces
+{
+    public interface IUserRecipeRepository
+    {
+        Task<UserRecipe?> GetUserRecipeAsync(string userId, int recipeId);
+        Task AddUserRecipeAsync(UserRecipe userRecipe);
+        Task UpdateUserRecipeAsync(UserRecipe userRecipe);
+        Task<IEnumerable<UserRecipe>> GetFavoritesByUserAsync(string userId);
+    }
+
+}
