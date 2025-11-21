e3297b55da608cff365f520c6f600bcb450acb6a
MJ Eng <mj.eng.contact@gmail.com>
2025-11-21 19:57:34 +0100

    add required to admin dto

--- J3M.Shared/DTOs/Admin/AdminLoginDTO.cs
+++ J3M.Shared/DTOs/Admin/AdminLoginDTO.cs
@@ -1,8 +1,12 @@
-﻿namespace J3M.Shared.DTOs.Admin
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace J3M.Shared.DTOs.Admin
 {
     public class AdminLoginDTO
     {
+        [Required]
         public string Username { get; set; }
+        [Required]
         public string Password { get; set; }
     }
 }
--- J3M.Shared/DTOs/Admin/AdminRegisterDTO.cs
+++ J3M.Shared/DTOs/Admin/AdminRegisterDTO.cs
@@ -1,8 +1,12 @@
-﻿namespace J3M.Shared.DTOs.Admin
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace J3M.Shared.DTOs.Admin
 {
     public class AdminRegisterDTO
     {
+        [Required]
         public string Username { get; set; }
+        [Required]
         public string Password { get; set; }
     }
 }
